0f4d9370655c15c4655e9945fa8aa153c67aa5e0
natakozlova <natabalabanova555@yandex.ru>
2023-09-16 21:27:04 +0300

    add function

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,39 @@
-function compareArrays(arr1, arr2) {
-  
-}
+  function compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+
+    return arr1.every((value, index) => value === arr2[index]);
+  }
+
+  const array1 = [0, 6, -1];
+  const array2 = [0, 6, -1];
+  console.log(compareArrays(array1, array2));
+
+  const array3 = [6, 2, 3];
+  const array4 = [5, 2, 1];
+  console.log(compareArrays(array3, array4));
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  const averageAge = totalAge / filteredUsers.length;
+
+  return averageAge;
 }
+
+const users = [
+  { name: 'Maria', age: 45, gender: 'female' },
+  { name: 'Mark', age: 53, gender: 'male' },
+  { name: 'Nick', age: 21, gender: 'male' },
+  { name: 'Polina', age: 54, gender: 'female' },
+  { name: 'Filipp', age: 34, gender: 'male' }
+];
+
+console.log(getUsersNamesInAgeRange(users, 'female'));
+console.log(getUsersNamesInAgeRange(users, 'male'));
