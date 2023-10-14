b25db2591d33ab50d237103f2f4aec9732aa0cd9
natakozlova <natabalabanova555@yandex.ru>
2023-10-14 09:49:28 +0300

    remove unnecessary

--- 2.functions/tests.js
+++ 2.functions/tests.js
@@ -87,19 +87,3 @@ describe("Домашнее задание к занятию 2 Функции", (
     });
   });
 });
-function testCase() {
-  const a = 1;
-  const b = -3;
-  const c = 2;
-
-  const solutions = solveEquation(a, b, c);
-  if (solutions[0] === 2 && solutions[1] === 1) {
-    console.log("Тест пройден");
-  } else {
-    console.log("Тест не пройден");
-  }
-}
-
-testCase();
-
-
